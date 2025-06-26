41f02a796eb15853eba43ab98c5d0a88cdfb0b96
Romel Romero <romelromero123@hotmail.com>
2025-06-26 19:34:01 +0200

    Primeros cambios

--- routes/routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import EmailController from "../controllers/EmailController.js";
-
-const router = express.Router();
-
-router.post("/email", EmailController); 
-
-export default router;
