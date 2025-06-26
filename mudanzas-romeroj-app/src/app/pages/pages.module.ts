3ca7a345adab872667c558ac90d2c165f8220831
Romel Romero <romelromero123@hotmail.com>
2025-06-26 19:53:01 +0200

    Primeros cambios

--- mudanzas-romeroj-app/src/app/pages/pages.module.ts
+++ mudanzas-romeroj-app/src/app/pages/pages.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from '../components/header/header.component';
-import { CarouselModule } from 'ngx-owl-carousel-o';
 import { WhoWeAreComponent } from './who-we-are/whoweare.component';
 import { MudanzasComponent } from './mudanzas/mudanzas.component';
 import { MudanzasHogarComponent } from './mudanzas/hogar/mudanzas-hogar.component';
@@ -14,7 +13,6 @@ import { HttpClientModule } from '@angular/common/http';
   declarations: [],
   imports: [
     CommonModule, 
-    CarouselModule,
     HomeComponent,
     WhoWeAreComponent,
     MudanzasComponent,
